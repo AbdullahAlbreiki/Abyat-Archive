393d0640b8a495c6cddd7c06c6940dafb3583137
Abdullah Albreiki <abdallahalbraiky@gmail.com>
2025-08-10 21:03:47 +0400

    scripts first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const users = {
+    user:{
+        id:"987654321",
+        name:"Abdullah Albreiki",
+        email:"abdallahalbraiky@gmail.com",
+        abyat:{
+            author:"",
+            category:"",
+            
+        }
+    }
+}
